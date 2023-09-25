b6aad705994cd10a18e3940507f3a4874c0e8dba
Stavros <sgerolymatos1@gmail.com>
2023-09-25 12:41:54 +0200

    initial commit

---
+++ training.py
@@ -0,0 +1,102 @@
+import dgl
+import torch
+import random
+import os
+import numpy as np
+import networkx as nx
+import torch.nn as nn
+import torch.nn.functional as F
+
+from collections import OrderedDict, defaultdict
+from dgl.nn.pytorch import GraphConv
+from itertools import chain, islice, combinations
+from networkx.algorithms.approximation.independent_set import maximum_independent_set as mis
+from time import time
+
+seed_value = 1
+random.seed(seed_value)        # seed python RNG
+np.random.seed(seed_value)     # seed global NumPy RNG
+torch.manual_seed(seed_value)  # seed torch RNG
+
+# Set GPU/CPU
+TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+TORCH_DEVICE = 'cpu'
+TORCH_DTYPE = torch.float32
+print(f'Will use device: {TORCH_DEVICE}, torch dtype: {TORCH_DTYPE}')
+
+from utils import generate_graph, get_gnn, run_gnn_training, qubo_dict_to_torch, gen_combinations, loss_func
+from lp_to_bqm import BQM, qubo_to_torch
+# from mis_utils import gen_q_dict_mis, postprocess_gnn_mis
+
+
+# Graph hypers
+d = 3
+p = None
+graph_type = 'complete'
+
+# NN learning hypers #
+number_epochs = int(1e5)
+learning_rate = 1e-4
+PROB_THRESHOLD = 0.5
+
+# Early stopping to allow NN to train to near-completion
+tol = 1e-4          # loss must change by more than tol, or trigger
+patience = 100    # number early stopping triggers before breaking loop
+
+bqm_model = BQM()
+bqm_model.parse_lp("model.lp")
+
+# print(bqm_model.constraints_dict)
+# print(bqm_model.variables)
+# print(bqm_model.quadratic)
+Q, _ = qubo_to_torch(bqm_model, eq_inf=50, leq_infinity=100) # pre-constrained Q matrix
+
+
+
+# Problem size (e.g. graph size)
+n = Q.shape[0] # number of nodes
+
+# Establish dim_embedding and hidden_dim values
+dim_embedding = int(np.sqrt(n))    # e.g. 10
+hidden_dim = int(dim_embedding/2)  # e.g. 5
+
+
+# Constructs a random d-regular or p-probabilistic graph
+nx_graph = generate_graph(n=n, d=d, p=p, graph_type=graph_type, random_seed=seed_value)
+# get DGL graph from networkx graph, load onto device
+graph_dgl = dgl.from_networkx(nx_graph=nx_graph)
+graph_dgl = graph_dgl.to(TORCH_DEVICE)
+# Construct Q matrix for graph
+
+
+# Establish pytorch GNN + optimizer
+opt_params = {'lr': learning_rate}
+gnn_hypers = {
+    'dim_embedding': dim_embedding,
+    'hidden_dim': hidden_dim,
+    'dropout': 0.0,
+    'number_classes': 1,
+    'prob_threshold': PROB_THRESHOLD,
+    'number_epochs': number_epochs,
+    'tolerance': tol,
+    'patience': patience
+}
+
+net, embed, optimizer = get_gnn(n, gnn_hypers, opt_params, TORCH_DEVICE, TORCH_DTYPE)
+
+# For tracking hyperparameters in results object
+gnn_hypers.update(opt_params)
+
+print('Running GNN...')
+gnn_start = time()
+
+_, epoch, final_bitstring, best_bitstring = run_gnn_training(
+    Q, graph_dgl, net, embed, optimizer, gnn_hypers['number_epochs'],
+    gnn_hypers['tolerance'], gnn_hypers['patience'], gnn_hypers['prob_threshold'])
+
+gnn_time = time() - gnn_start
+
+
+final_loss = loss_func(final_bitstring.float(), Q)
+final_bitstring_str = ','.join([str(x) for x in final_bitstring])
+z = final_bitstring_str
